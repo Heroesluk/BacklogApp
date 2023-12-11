22af389826599b0832cf55f004bc8c02064f242f
heroesluk <242209@edu.p.lodz.pl>
2023-12-11 18:36:18 +0000

    works!

--- app/src/main/java/template/MainActivity.kt
+++ app/src/main/java/template/MainActivity.kt
@@ -12,17 +12,17 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import dagger.hilt.android.AndroidEntryPoint
 import template.theme.TemplateTheme
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        configureEdgeToEdgeWindow()
 
         setContent {
-            ConfigureTransparentSystemBars()
-
             TemplateTheme {
                 Surface(
                     color = MaterialTheme.colorScheme.background,
@@ -33,35 +33,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    /**
-     * Configures our [MainActivity] window so that it reaches edge to edge of the device, meaning
-     * content can be rendered underneath the status and navigation bars.
-     *
-     * This method works hand in hand with [ConfigureTransparentSystemBars], to make sure content
-     * behind these bars is visible.
-     *
-     * Keep in mind that if you need to make sure your content padding doesn't clash with the status bar text/icons,
-     * you can leverage modifiers like `windowInsetsPadding()` and `systemBarsPadding()`. For more information,
-     * read the Compose WindowInsets docs: https://developer.android.com/reference/kotlin/androidx/compose/foundation/layout/WindowInsets
-     */
-    private fun configureEdgeToEdgeWindow() {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-    }
-
-    @Composable
-    private fun ConfigureTransparentSystemBars() {
-        val systemUiController = rememberSystemUiController()
-        val useDarkIcons = !isSystemInDarkTheme()
-
-        DisposableEffect(systemUiController, useDarkIcons) {
-            systemUiController.setSystemBarsColor(
-                color = Color.Transparent,
-                darkIcons = useDarkIcons,
-            )
-
-            onDispose { }
-        }
-    }
 }
 
 @Composable
--- app/src/main/java/template/theme/Theme.kt
+++ app/src/main/java/template/theme/Theme.kt
@@ -2,41 +2,29 @@ package template.theme
 
 import android.annotation.TargetApi
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
 
-private val darkColorScheme = darkColorScheme(
-    primary = Purple200,
-    secondary = Teal200,
-)
 
-private val lightColorScheme = lightColorScheme(
-    primary = Purple500,
-    secondary = Teal200,
+private val DarkColorPalette = darkColorScheme(
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = Color.Cyan,
+    onSurface = DarkGray,
 )
 
 @Composable
 @TargetApi(Build.VERSION_CODES.S)
 fun TemplateTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicTheme: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicTheme && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
-        dynamicTheme && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
-        darkTheme -> darkColorScheme
-        else -> lightColorScheme
-    }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = DarkColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content,
