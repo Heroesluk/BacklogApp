d94c26bd3e62ab079c7a516c72221617404e5343
heroesluk <242209@edu.p.lodz.pl>
2023-12-16 14:35:24 +0000

    tests broken cause of corutines

--- app/src/androidTest/java/template/data/DaoTest.kt
+++ app/src/androidTest/java/template/data/DaoTest.kt
@@ -4,6 +4,14 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -17,7 +25,10 @@ import java.io.IOException
 class SimpleEntityReadWriteTest {
     private lateinit var placeDao: PlaceDao
     private lateinit var db: PlaceDatabase
+    private val testDispatcher = StandardTestDispatcher()
 
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun createDb() {
         val context = ApplicationProvider.getApplicationContext<Context>()
@@ -25,6 +36,9 @@ class SimpleEntityReadWriteTest {
             context, PlaceDatabase::class.java,
         ).build()
         placeDao = db.placeDao
+
+        Dispatchers.setMain(testDispatcher)
+
     }
 
     @After
@@ -33,34 +47,41 @@ class SimpleEntityReadWriteTest {
         db.close()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        testDispatcher.cancel()
+    }
+
+
     @Test
     @Throws(Exception::class)
-    fun insertPlace_createPlace() {
+    fun insertPlace_createPlace() = runTest {
         placeDao.insertPlace(Place("ok", "ok", "ok", 1, 1, "aaa"))
         placeDao.insertPlace(Place("new", "ok", "ok", 1, 1, "aaa"))
-
-        Assert.assertEquals(2, placeDao.getPlaces().size)
+        val afterSize = placeDao.getPlaces().toList().size
+        Assert.assertEquals(2, afterSize)
 
     }
 
     @Test
     @Throws(Exception::class)
-    fun deletePlace_deletePlace() {
-        Assert.assertEquals(0, placeDao.getPlaces().size)
+    fun deletePlace_deletePlace() = runTest {
 
 
         val placeId = placeDao.insertPlace(Place("sex", "ok", "ok", 1, 1, "aaa"))
 
         placeDao.deletePlace(placeId)
-
-        Assert.assertEquals(0, placeDao.getPlaces().size)
+        val afterSize = placeDao.getPlaces().toList().size
+        Assert.assertEquals(0, afterSize)
 
     }
 
 
     @Test
     @Throws(Exception::class)
-    fun updatePlace_returnOneOnSuccessfulUpdate() {
+    fun updatePlace_returnOneOnSuccessfulUpdate() = runTest {
         placeDao.insertPlace(Place("ok", "ok", "ok", 1, 1, "aaa"))
         val p1 = placeDao.updatePlace(Place(1, "newName", "ok", "ok", 1, 1, "aaa"))
         Assert.assertEquals(p1, 1)
@@ -69,7 +90,7 @@ class SimpleEntityReadWriteTest {
 
     @Test
     @Throws(Exception::class)
-    fun updatePlace_returnZeroOnUnsuccessfulUpdate() {
+    fun updatePlace_returnZeroOnUnsuccessfulUpdate() = runTest {
         val p1 = placeDao.updatePlace(Place(1, "newName", "ok", "ok", 1, 1, "aaa"))
         Assert.assertEquals(p1, 0)
     }
--- app/src/androidTest/java/template/data/UseCaseTest.kt
+++ app/src/androidTest/java/template/data/UseCaseTest.kt
@@ -3,6 +3,8 @@ package template.data
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
@@ -25,16 +27,16 @@ class UseCaseTest {
 
     @Test
     @Throws(Exception::class)
-    fun placeBasicUseCaseTest() {
+    fun placeBasicUseCaseTest() = runTest {
         hiltRule.inject()
 
         val place = Place("eiffel Tower", "desc", "2022/12/11", 5, 0, "imgid");
         val placeId = placeUseCases.addPlace(place)
-        Assert.assertEquals(1, placeUseCases.getPlaces().size)
+        Assert.assertEquals(1, placeUseCases.getPlaces().toList().size)
         Assert.assertEquals(placeUseCases.getPlace(placeId)!!.name, place.name)
 
         placeUseCases.deletePlace(placeId)
 
-        Assert.assertEquals(placeUseCases.getPlaces().size, 0)
+        Assert.assertEquals(placeUseCases.getPlaces().toList().size, 0)
     }
 }
