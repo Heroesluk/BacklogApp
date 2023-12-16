ade269bcc18bb0812bae77da04923bc22f32acbb
heroesluk <242209@edu.p.lodz.pl>
2023-12-16 12:21:59 +0000

    why i didint commit this

---
+++ app/src/main/java/template/domain/model/place.kt
@@ -0,0 +1,21 @@
+package template.domain.model
+
+import androidx.annotation.DrawableRes
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class Place(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val description: String,
+    val date: String,
+    val score: Int,
+    val locationId: Int,
+    val imageFileName: String,
+) {
+    constructor(name: String, description: String, date: String, score: Int, locationId: Int, imageFileName: String)
+        : this(0, name, description, date, score, locationId, imageFileName)
+
+}
