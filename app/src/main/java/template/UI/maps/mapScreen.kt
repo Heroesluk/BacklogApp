3d70bf01249ed5dd2db9d807e1e9aab2609186dc
heroesluk <242209@edu.p.lodz.pl>
2023-12-25 14:33:57 +0000

    added top bar

--- app/src/main/java/template/UI/maps/mapScreen.kt
+++ app/src/main/java/template/UI/maps/mapScreen.kt
@@ -1,12 +1,21 @@
 package template.UI.maps
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -21,7 +30,11 @@ import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import template.UI.Screen
+import template.UI.places.components.filterMenuButton
+import template.UI.places.components.sortMenuButton
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun mapScreen(
     navController: NavController,
@@ -39,11 +52,30 @@ fun mapScreen(
 
     places.add(PlaceLocation(false, MarkerState(LatLng(1.34, 103.89))))
 
-    Box() {
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState,
-        ) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Places you've visited")
+                },
+                actions = {
+                    Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                        filterMenuButton()
+                        sortMenuButton(sortFun = { })
+
+                    }
+                },
+
+                )
+        },
+    ) { it ->
+        Log.i("a",it.toString())
+
+        Box() {
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState = cameraPositionState,
+            ) {
 ////            Marker(
 ////                state = MarkerState(position = singapore),
 ////                title = "Singapore",
@@ -69,40 +101,44 @@ fun mapScreen(
 ////            )
 
 
-            places.forEach {
-                Marker(
-                    state = it.markerState,
-                    draggable = it.draggable,
-                )
+                places.forEach {
+                    Marker(
+                        state = it.markerState,
+                        draggable = it.draggable,
+                    )
+                }
             }
-        }
 
-        Box(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(0.dp, 0.dp, 0.dp, 20.dp),
-        ) {
-            Button(
-                onClick = {
-                    Log.i("aa", places.toString())
 
-                    if (places.last().draggable == false) {
-                        places.add(PlaceLocation(true, MarkerState(LatLng(cameraPos.latitude, cameraPos.longitude))))
-                        buttonState.value = "Submit new marker"
-                    } else {
-                        buttonState.value = "Add new marker"
-                        val temp = places.last()
-                        places.removeLast();
-                        places.add(PlaceLocation(false, temp.markerState))
-                    }
-
-                },
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(0.dp, 0.dp, 0.dp, 35.dp),
             ) {
-                Text(buttonState.value)
+                Button(
+                    onClick = {
+                        Log.i("aa", places.toString())
+
+                        if (places.last().draggable == false) {
+                            places.add(PlaceLocation(true, MarkerState(LatLng(cameraPos.latitude, cameraPos.longitude))))
+                            buttonState.value = "Submit new marker"
+                        } else {
+                            buttonState.value = "Add new marker"
+                            val temp = places.last()
+                            places.removeLast();
+                            places.add(PlaceLocation(false, temp.markerState))
+                        }
+
+                    },
+                ) {
+                    Text(buttonState.value)
+                }
             }
         }
 
     }
+
+
 }
 
 
