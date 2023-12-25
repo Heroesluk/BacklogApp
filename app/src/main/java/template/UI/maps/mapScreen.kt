2ee7b916f964bdf2a4da92f1115555cf94abf5ff
heroesluk <242209@edu.p.lodz.pl>
2023-12-25 14:17:23 +0000

    adding works correctly for marker

--- app/src/main/java/template/UI/maps/mapScreen.kt
+++ app/src/main/java/template/UI/maps/mapScreen.kt
@@ -1,97 +1,112 @@
 package template.UI.maps
 
-import androidx.compose.foundation.background
+import android.util.Log
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerComposable
-import com.google.maps.android.compose.MarkerInfoWindow
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
-import kotlinx.coroutines.flow.map
-import template.UI.places.PlacesViewModel
-import template.domain.model.Place
-import template.domain.usecase.PlaceUseCases
-import template.domain.util.SortBy
-import template.domain.util.SortDirection
-import template.util.FilterSort
 
 @Composable
 fun mapScreen(
     navController: NavController,
 ) {
 
-    val singapore = LatLng(1.35, 103.87)
+    val cameraPos = LatLng(1.35, 103.87)
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        position = CameraPosition.fromLatLngZoom(cameraPos, 10f)
     }
 
 
+    val places = remember { mutableStateListOf<PlaceLocation>() }
 
+    var buttonState = remember { mutableStateOf("Add new marker") }
 
-    val places = remember { mutableListOf<PlaceLocation>() }
-
-    places.add(PlaceLocation(1.35f,103.95f))
+    places.add(PlaceLocation(false, MarkerState(LatLng(1.34, 103.89))))
 
     Box() {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState,
         ) {
-            Marker(
-                state = MarkerState(position = singapore),
-                title = "Singapore",
-                snippet = "Marker in Singapore",
-                
-            )
+////            Marker(
+////                state = MarkerState(position = singapore),
+////                title = "Singapore",
+////                snippet = "Marker in Singapore",
+////
+////            )
+//
+////            MarkerInfoWindow(
+////                state = MarkerState(
+////                    position = LatLng(1.35, 103.90),
+////                ),
+////                draggable = true,
+////                content = {
+////                    Box(modifier = Modifier.background(Color.Black)) {
+////                        Text("Eiffel tower")
+////                    }
+////                },
+////                onClick = { it ->
+////
+////                    it.showInfoWindow()
+////                    true
+////                },
+////            )
 
-            MarkerInfoWindow(
-                state = MarkerState(
-                    position = LatLng(1.35, 103.90),
-                ),
-                draggable = true,
-                content = {
-                    Box(modifier = Modifier.background(Color.Black)) {
-                        Text("Eiffel tower")
-                    }
-                },
-                onClick = { it ->
 
-                    it.showInfoWindow()
-                    true
-                },
-            )
+            places.forEach {
+                Marker(
+                    state = it.markerState,
+                    draggable = it.draggable,
+                )
+            }
         }
 
-        Box(modifier = Modifier.align(Alignment.BottomCenter).padding(0.dp,0.dp,0.dp,20.dp)) {
-            Button(onClick = {}){
-                Text("Add new marker")
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(0.dp, 0.dp, 0.dp, 20.dp),
+        ) {
+            Button(
+                onClick = {
+                    Log.i("aa", places.toString())
+
+                    if (places.last().draggable == false) {
+                        places.add(PlaceLocation(true, MarkerState(LatLng(cameraPos.latitude, cameraPos.longitude))))
+                        buttonState.value = "Submit new marker"
+                    } else {
+                        buttonState.value = "Add new marker"
+                        val temp = places.last()
+                        places.removeLast();
+                        places.add(PlaceLocation(false, temp.markerState))
+                    }
+
+                },
+            ) {
+                Text(buttonState.value)
             }
         }
 
     }
-
-
 }
 
 
 data class PlaceLocation(
-    val lat: Float,
-    val long: Float,
+    val draggable: Boolean,
+    val markerState: MarkerState,
 )
