a0a4205b03077c1075176cc0c8bd7c00636957d4
242209 <242209@edu.p.lodz.pl>
2024-01-06 22:50:05 +0100

    works

--- app/src/main/java/template/UI/asistant/assistantViewModel.kt
+++ app/src/main/java/template/UI/asistant/assistantViewModel.kt
@@ -7,26 +7,25 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import template.domain.usecase.PlaceUseCases
+import javax.inject.Inject
 import com.hexascribe.vertexai.VertexAI
 import com.hexascribe.vertexai.domain.VertexResult
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import dagger.hilt.android.lifecycle.HiltViewModel
 import io.ktor.client.HttpClient
 import io.ktor.client.request.header
 import io.ktor.client.request.request
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
 import io.ktor.http.HttpMethod
+import io.ktor.http.contentType
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import template.domain.usecase.PlaceUseCases
-import template.util.Contents
-import template.util.Output
-import template.util.Place
-import javax.inject.Inject
+import org.json.JSONObject
+import com.google.gson.JsonSerializer
 
 
 @HiltViewModel
@@ -45,7 +44,7 @@ class AssistantViewModel @Inject constructor(
 
     val vertexAI by lazy {
         VertexAI.Builder()
-            .setAccessToken("ya29.a0AfB_byAP7RQheU5plhem7nknZMkpardqvwQhz-JDgK8YMl9keAXcQVGhe52xc_g6LnqrZMvbLBkCrK72F6ALUbR7rlHoEpPMqbuK1Qqq95h5cFifwTdxzAURNFkoN_QUAGk8MW6A8pYJhzUifoW23icLa0LGJ-fqtI2kpAaCgYKAVcSARESFQHGX2MiO5zDVUw_OaFaHHqJ0HmPpg0173")
+            .setAccessToken("ya29.a0AfB_byBnKjVRyyCUNjgsHgQlE4yN9V17yW-SaJcegwlCQpbkQfM4hNuLHFyBVkdKdbcDXQX_RqlaqXbPUXy0FzovCJJInfU8vA_mbeqg--izmO4pMK1zCG5iK8d9o8zAQHxkbM0E3jfxVhjCpzPuHJpYo7wE1pglyIbATwaCgYKAdoSARESFQHGX2Mi0P5htl1ySewCLQ6hiJXf5w0173")
             .setProjectId("ageless-webbing-405115")
             .build()
     }
@@ -55,14 +54,6 @@ class AssistantViewModel @Inject constructor(
             .setMaxTokens(256)
     }
 
-    val places = listOf(
-        Place("Im going to ask you to write me down a list of places to visit in specific location - town or city. Your response should be formatted in a way: PlaceName - PlaceDescription description should be around 7-20 words. can you suggest me 5 places to visit in Berlin")
-    )
-
-    val contents = Contents("user", places)
-    val out = Output(contents)
-
-    val moshi: Moshi = Moshi.Builder().build()
 
 
 
@@ -73,36 +64,15 @@ class AssistantViewModel @Inject constructor(
     fun request() = viewModelScope.launch {
         val prompt =
             """I'm going to ask you to write me down a list of places to visit in specific location - town or city. Your response should be formatted in a way:
-                PlaceName - PlaceDescription; 
-                description should be around 7-20 words. 
-                can you suggest me 5 places to visit in Berlin"""
-//        Log.i("prompt: ", prompt)
-        Log.i("promt", body.toString())
+    PlaceName - PlaceDescription end with semicolon, dont use asterix
+    description should be around 7-20 words. 
+    can you suggest me 5 places to visit in $townInput"""
 
-//        val result = textRequest.execute(prompt)
-        val response: HttpResponse =
-            client.request("https://us-central1-aiplatform.googleapis.com/v1/projects/ageless-webbing-405115/locations/us-central1/publishers/google/models/gemini-pro-vision:streamGenerateContent") {
-                method = HttpMethod.Post
-                header(
-                    "Authorization",
-                    "Bearer ya29.a0AfB_byCCbrH9CukGYR53jhYf0KIQ6jFv1f30E9KsPBvSObBMoyYwj_qb-o9nUnf8lrLJ0Cp3pbIipnrMaxURrUYW6o20q6hFwdrNobd4QjSvxYvou5tnbteA0mk6Rqnwf4-l_UJLtVjMD_GsIBg11kXPLrD2dedUHpci1AaCgYKAY4SARESFQHGX2Mi3ygFiwJqSBdZJ7i8nrpSQQ0173",
-                )
-                setBody("""{
-  "contents": {
-    "role": "user",
-    "parts": [
-      {
-        "text": "Im going to ask you to write me down a list of places to visit in specific location - town or city. Your response should be formatted in a way: PlaceName - PlaceDescription description should be around 7-20 words. can you suggest me 5 places to visit in Berlin"
-      }
-    ]
-  }
-}""")
-                Log.i("a", this.body.toString())
-            }
+        val result = textRequest.execute(prompt)
 
-        Log.i("response: ", response.bodyAsText())
 
-//        handleTextResult(result)
+
+        handleTextResult(result)
     }
 
     fun onTownFieldChange(newTown: String) {
